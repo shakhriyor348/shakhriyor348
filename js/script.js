f72cbec4703396e54306d62092f374f04589cb04
shakhriyor <andrian.jr@mail.ru>
2020-03-31 21:33:31 +0300

    test

---
+++ js/script.js
@@ -0,0 +1,10 @@
+(function($) {
+    $(function() {
+    
+      $('#up').click(function() {
+        $('html, body').animate({scrollTop: 0},1000);
+        return false;
+      })
+    
+    })
+    })(jQuery)
